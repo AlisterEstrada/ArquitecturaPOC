0725d6c3518e504c391e51874e070f2ee24acc3f
AlisterEstrada <alister.estrada@gmail.com>
2020-11-25 18:33:03 -0600

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,31 @@ import { TestComponent } from './test/test.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ClassroomFormComponent } from './classroom-form/classroom-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SalonesComponent } from './salones/salones.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    ClassroomFormComponent
+    ClassroomFormComponent,
+    LoginComponent,
+    SalonesComponent
   ],
   imports: [
-    BrowserModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserModule,
+    RouterModule.forRoot([
+    {path: 'salones', component: SalonesComponent},
+    {path: 'login' , component: LoginComponent},
+    {path: '**', component: SalonesComponent},
+
+  ]),
+    AppRoutingModule,
   ],
   providers: [ClassroomService],
   bootstrap: [AppComponent]
