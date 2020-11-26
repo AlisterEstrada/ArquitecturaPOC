92a16644ac697e5ed14f00487cec83ac905cd32d
AlisterEstrada <alister.estrada@gmail.com>
2020-11-25 18:52:12 -0600

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -26,20 +26,7 @@ export class LoginComponent implements OnInit {
     var raw = JSON.stringify({"User":"A01336767"});
 
     console.log(raw);
-    var alumno = [];
-    // create a JSON object with parameters for API call and store in a variable
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw
-    };
-
-    // make API call with parameters and use promises to get response
-     fetch("https://l64x96k0ta.execute-api.us-west-2.amazonaws.com/dev", requestOptions)
-    .then(response => response.json());
-
-
-
+  
 
     }
 
